fe9755dd7fa099e9514b87a285fe508492f64373
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-12-03 21:18:57 +0200

    add dto models

---
+++ app/src/main/java/com/fighter/newsapp/data/remote/model/NewsResponse.kt
@@ -0,0 +1,9 @@
+package com.fighter.newsapp.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class NewsResponse(
+    @SerializedName("status") val status: String,
+    @SerializedName("totalResults") val totalResults: Int,
+    @SerializedName("articles") val articles: List<Article>,
+)
