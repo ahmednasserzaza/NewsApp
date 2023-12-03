a11416774eca1cc820292600be76582a86325883
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-12-03 23:03:03 +0200

    create NetworkModule dependency injection

---
+++ app/src/main/java/com/fighter/newsapp/di/NetworkModule.kt
@@ -0,0 +1,54 @@
+package com.fighter.newsapp.di
+
+import com.fighter.newsapp.data.remote.AuthInterceptor
+import com.fighter.newsapp.data.remote.service.NewsService
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val baseUrl = "https://newsapi.org/v2/"
+    @Singleton
+    @Provides
+    fun provideNewsService(retrofit: Retrofit): NewsService {
+        return retrofit.create(NewsService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient, gsonConverterFactory: GsonConverterFactory): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson {
+        return Gson()
+    }
+}
